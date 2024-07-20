069c866f8c13373069920def764090bf05f66ab8
Mirco Stelzer <mirco.stelzer@studenti.unitn.it>
2024-07-20 15:19:15 +0200

    improvement of cards game + lecture files

---
+++ Programmazione 2/IntelliJ/CarteFx/src/main/java/Deck.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        for (Card.Suit s : Card.Suit.values()) {
+            for (Card.Rank r : Card.Rank.values()) {
+                for (int i=0; i<2; i++) {
+                    cards.add(new Card(s, r));
+                }
+            }
+        }
+    }
+
+    public void shuffle() {
+        System.out.println("========================");
+        System.out.println("Shuffling Deck");
+        for (int i=0; i<100; i++) {
+            Random rand = new Random();
+            Card temp = cards.remove(rand.nextInt(cards.size()));
+            cards.add(rand.nextInt(cards.size()),temp);
+        }
+        System.out.println("========================");
+    }
+
+    public void print() {
+        for (Card c : cards) {
+            c.print();
+        }
+    }
+
+    public void deal(int n) {
+        for (int i=0; i<n; i++) {
+            Card show = cards.get(i);
+            show.print();
+        }
+    }
+
+    public Card getCard(int i) {
+        return cards.get(i);
+    }
+}
---
+++ Programmazione 2/IntelliJ/CarteFx/src/main/java/Main.java
@@ -0,0 +1,104 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Deck deck = new Deck();
+        HBox box = new HBox();
+        AnchorPane root = new AnchorPane();
+        Button shuffle = new Button("Shuffle");
+        Button deal = new Button("Deal");
+        box.getChildren().addAll(shuffle, deal);
+        box.setSpacing(10);
+        box.setAlignment(Pos.BOTTOM_CENTER);
+        Image image = new Image("file:/home/mirco/Downloads/4102018carteramino3.jpg");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(250);
+        imageView.setFitHeight(200);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
+        imageView.setCache(true);
+        imageView.setX(120);
+        imageView.setY(80);
+        root.setBackground(new Background(new BackgroundFill(Paint.valueOf("ROYALBLUE"), new CornerRadii(0), new Insets(0))));
+        root.getChildren().addAll(box, imageView);
+        AnchorPane.setBottomAnchor(box, 100.0);
+        AnchorPane.setLeftAnchor(box, 180.0);
+
+        Scene scene = new Scene(root, 500, 400, Color.ROYALBLUE);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Cards");
+        primaryStage.show();
+
+        shuffle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                                      @Override
+                                      public void handle(MouseEvent mouseEvent) {
+                                          deck.shuffle();
+                                      }
+        });
+        deal.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent mouseEvent) {
+                deck.deal(10);
+                boolean win = false;
+
+                Text text = new Text();
+                StackPane root1 = new StackPane(text);
+                Scene scene1 = new Scene(root1, 200, 150);
+                Stage stage1 = new Stage();
+
+                for (int i = 0; i < 10 && !win; i++) {
+                    Card curr = deck.getCard(i);
+                    for (int j = 0; j < i; j++) {
+                        Card check = deck.getCard(j);
+                        if (curr.equals(check)) {
+                            text.setText("Hai vinto!");
+                            text.setTextAlignment(TextAlignment.CENTER);
+                            text.setFont(Font.font("Arial"));
+                            text.setFont(new Font(24));
+                            root1.setBackground(new Background(new BackgroundFill(Paint.valueOf("GREEN"), new CornerRadii(0), new Insets(0))));
+                            stage1.setScene(scene1);
+                            stage1.show();
+                            primaryStage.close();
+                            win = true;
+                        }
+                    }
+                }
+                if (!win) {
+                    text.setText("Hai perso");
+                    text.setFill(Color.BLACK);
+                    text.setFont(Font.font("Arial"));
+                    text.setFont(Font.font(24));
+                    text.setTextAlignment(TextAlignment.CENTER);
+                    root1.setBackground(new Background(new BackgroundFill(Paint.valueOf("RED"), new CornerRadii(0), new Insets(0))));
+                    stage1.setScene(scene1);
+                    stage1.sizeToScene();
+                    stage1.show();
+                    primaryStage.close();
+
+                }
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
