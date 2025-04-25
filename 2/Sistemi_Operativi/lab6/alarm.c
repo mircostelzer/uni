c248a172d919d6a7cdb20946dec93564c28f1397
Mirco Stelzer <mirco.stelzer@studenti.unitn.it>
2025-04-25 17:21:37 +0200

    Exercises and examples

---
+++ 2/Sistemi_Operativi/lab6/alarm.c
@@ -0,0 +1,13 @@
+#include <signal.h> <stdio.h> <stdlib.h> <unistd.h> //alarm.c
+
+short cnt = 0;
+void myHandler(int sigNum){printf("ALARM!\n"); cnt++;}
+
+int main(){
+	signal(SIGALRM,myHandler);
+	alarm(0); //Clear any pending alarm
+	alarm(5); //Set alarm in 5 seconds
+	//Set new alarm (cancelling previous one)
+	printf("Seconds remaining to previous alarm %d\n", alarm(2));
+	while(cnt<1);
+}
